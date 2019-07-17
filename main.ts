c5d405e1cb9a6db5cc59d278082ba9e97b335893
chengy17 <chengy_95@163.com>
2019-07-17 14:18:23 +0800

    change icon and color

--- main.ts
+++ main.ts
@@ -5,7 +5,7 @@ load dependency
 "OmniBit": "file:../pxt-OmniBit"
 */
 
-//% color="#ECA40D" weight=20 icon="\uf085"
+//% color="#1E90FF" weight=20 icon="\uf0d1"
 namespace OmniBit {
 
     const PCA9685_ADD = 0x40
