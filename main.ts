371f87ee159b9446bedb5ff760ffe111170eb390
chengy17 <chengy_95@163.com>
2019-07-18 17:08:09 +0800

    icon f1b9->f63b

--- main.ts
+++ main.ts
@@ -5,7 +5,7 @@ load dependency
 "OmniBit": "file:../pxt-OmniBit"
 */
 
-//% color="#ECA40D" weight=20 icon="\uf1b9"
+//% color="#ECA40D" weight=20 icon="\uf63b"
 namespace OmniBit {
 
     const PCA9685_ADD = 0x40
