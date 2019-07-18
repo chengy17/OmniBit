3e4a3b1de25b5ec72c44ab75251bd1ee7497ce6d
chengy17 <chengy_95@163.com>
2019-07-18 16:54:55 +0800

    color B2DFEE

--- main.ts
+++ main.ts
@@ -5,7 +5,7 @@ load dependency
 "OmniBit": "file:../pxt-OmniBit"
 */
 
-//% color="#62b900" weight=20 icon="\uf1b9"
+//% color="#B2DFEE" weight=20 icon="\uf1b9"
 namespace OmniBit {
 
     const PCA9685_ADD = 0x40
