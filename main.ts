bf332dd0b73e242769fc6eb13087f61fdff7d2cb
chengy17 <chengy_95@163.com>
2019-07-18 16:58:52 +0800

    color ECA40D

--- main.ts
+++ main.ts
@@ -5,7 +5,7 @@ load dependency
 "OmniBit": "file:../pxt-OmniBit"
 */
 
-//% color="#CD8500" weight=20 icon="\uf1b9"
+//% color="#ECA40D" weight=20 icon="\uf1b9"
 namespace OmniBit {
 
     const PCA9685_ADD = 0x40
